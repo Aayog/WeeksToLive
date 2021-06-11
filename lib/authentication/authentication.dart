a4bf2df1991d6f0c24fa57ad1851ea044983f0e8
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-06-11 18:40:05 +0545

    Added home and login screen

--- lib/authentication/authentication.dart
+++ lib/authentication/authentication.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:weekstolive/screens/home.dart';
 
 class AuthenticationWrapper extends StatelessWidget {
-  const AuthenticationWrapper({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return HomeScreen();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:weekstolive/authentication.dart';
+import 'package:weekstolive/authentication/authentication.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
