289814628ec5ee1066503f9e62f80ce520182ffe
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-06-19 14:29:30 +0545

    Added firestore in user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
+  final String id;
   final String name;
+  final String email;
   final String dob;
   final String sex;
 
   User({
+    required this.id,
     required this.name,
+    required this.email,
     required this.dob,
     required this.sex,
   });
+
+  DocumentReference get firestoreRef =>
+      FirebaseFirestore.instance.doc('users/$id');
+
+  Future<void> saveInfo() async {
+    await firestoreRef.set(toMap());
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'dob': dob,
+      'sex': sex,
+    };
+  }
 }
