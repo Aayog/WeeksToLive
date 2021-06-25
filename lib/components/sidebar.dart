c5d7c7b01295d2dd71cb81112bc5be0c1009c182
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-06-25 14:01:40 +0545

    Added option in side bar

--- lib/components/sidebar.dart
+++ lib/components/sidebar.dart
@@ -5,6 +5,36 @@ class SideBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          SizedBox(
+            height: 100.0,
+            child: DrawerHeader(
+              child: Text(
+                'Weeks to Live',
+                style: TextStyle(color: Colors.black, fontSize: 25),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.account_box),
+            title: Text('Profile'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {},
+          ),
+        ],
+      ),
+    );
   }
 }
