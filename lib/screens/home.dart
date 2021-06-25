84a76db732f5136c8725093439f0673236499f9f
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-06-25 14:39:38 +0545

    Relocated user details to profile screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:double_back_to_close_app/double_back_to_close_app.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:weekstolive/components/sidebar.dart';
 // import 'package:provider/provider.dart';
 
@@ -14,61 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final _auth = FirebaseAuth.instance;
-  CollectionReference users = FirebaseFirestore.instance.collection('users');
-
   @override
   Widget build(BuildContext context) {
-    String userId = _auth.currentUser!.uid;
-
-    Widget requestList() {
-      return FutureBuilder<DocumentSnapshot>(
-        future: users.doc(userId).get(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('Something went wrong!'),
-            );
-          }
-
-          if (snapshot.hasData) {
-            if (!snapshot.data!.exists) {
-              return Text("Document does not exist.");
-            }
-
-            if (snapshot.connectionState == ConnectionState.done) {
-              Map<String, dynamic> data =
-                  snapshot.data!.data() as Map<String, dynamic>;
-
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('Name: ${data['name']}'),
-                  SizedBox(height: 10),
-                  Text(
-                      'Date of Birth: ${DateFormat.yMd().format(data['dob'].toDate())}'),
-                  SizedBox(height: 10),
-                  Text('Gender: ${data['gender']}'),
-                  SizedBox(height: 10),
-                  Text('Do you smoke?: ${data['smoke']}'),
-                  SizedBox(height: 10),
-                  Text('Do you drink?: ${data['drink']}'),
-                  SizedBox(height: 10),
-                  Text('Email: ' + _auth.currentUser!.email!),
-                ],
-              );
-            }
-          }
-
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      );
-    }
-
     return Scaffold(
         appBar: AppBar(
           title: Text('Welcome to WeeksToLive'),
@@ -79,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
             content: Text('Press back twice to exit app.'),
           ),
           child: Center(
-            child: requestList(),
+            child: Text('Home'),
           ),
         ));
   }
